27fed0c917bc1aa6055c1f7826da405682ff2141
DylanSmith <dylan23smith1990@gmail.com>
2022-10-26 08:02:56 -0500

    migrated Models

--- AlgoBattles/settings.py
+++ AlgoBattles/settings.py
@@ -38,6 +38,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "battles.apps.BattlesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
--- battles/models.py
+++ battles/models.py
@@ -1,20 +1,29 @@
 from django.db import models
+from django.forms import EmailField
 
 # Create your models here.
 class User(models.Model):
+    id = models.IntegerField(primary_key=True)
     f_name=models.CharField(max_length=50)
     l_name=models.CharField(max_length=50)
-    email=models.CharField(max_length=100)
+    email=models.CharField(max_length=100, validators=[EmailField])#look into email validator
     password=models.CharField(max_length=30)
-    date_created = models.DateTimeField('sign up date')
+    date_created = models.DateTimeField(auto_now=True)
 
-# class Algo ?? 
-#   
-#  
-class Battle(models.Model): #possibly User Algo join
-    user1=models.CharField(max_length=50)
-    user2=models.CharField(max_length=50)
-    user3=models.CharField(max_length=50)
-    user4=models.CharField(max_length=50)
+class Problem(models.Model):
+    id = models.IntegerField(primary_key=True)
+    question= models.CharField(max_length=500)
+    test_file= models.CharField(max_length=500)
+  
+ 
+class Battle(models.Model): # User & Problem join
+    id = models.IntegerField(primary_key=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    problem= models.ForeignKey(Problem, on_delete=models.CASCADE)
 
+class Answer(models.Model): #user, problem, battle join
+    id = models.IntegerField(primary_key=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    problem= models.ForeignKey(Problem, on_delete=models.CASCADE)
+    solution = models.CharField(max_length=500)
 # class Winner ?? User & Battle Join table
