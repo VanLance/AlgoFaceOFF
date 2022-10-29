64fc5e32cc670e79e55060a78f443357895c3ab6
DylanSmith <dylan23smith1990@gmail.com>
2022-10-28 20:23:59 -0500

    update models & admin register Problem

--- battles/admin.py
+++ battles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Problem, Battle
+
+
+
+
 # Register your models here.
+
+admin.site.register(Problem)
--- battles/models.py
+++ battles/models.py
@@ -9,16 +9,23 @@ class User(models.Model):
     password=models.CharField(max_length=30)
     date_created = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f'{self.f_name} {self.l_name}'
+
 class Problem(models.Model):
     # id = models.IntegerField(primary_key=True)
     question= models.CharField(max_length=500)
     test_file= models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.question
   
- 
 class Battle(models.Model): # User & Problem join
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     problem= models.ForeignKey(Problem, on_delete=models.CASCADE)
-
+    time = models.IntegerField(blank=True, null=True)
+    # winner = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+ 
 class Answer(models.Model): #user, problem, battle join
     user=models.ForeignKey(User, on_delete=models.CASCADE)
     problem= models.ForeignKey(Problem, on_delete=models.CASCADE)
